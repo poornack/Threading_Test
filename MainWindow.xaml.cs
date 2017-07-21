fb35ffa623e0c588657aecfe0fa55d0dd3fa4547
Poorna Chander Kalidas <lola@lola.lola>
2017-07-20 23:49:45 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Threading_Test
+{
+
+    public class thread_test
+    {
+        
+        public void func1()
+        {
+
+            for(int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Thread operation cycle #" + i);
+            }
+
+        }
+
+
+    }
+      
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            thread_test var = new thread_test();
+
+            Thread t = new Thread(new ThreadStart(var.func1));
+
+            t.Start();
+
+            for(int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Main thread cycle #" + i);
+            }
+            
+        }
+    }
+}
